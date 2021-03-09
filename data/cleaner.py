5e0a33b6396b7a81941d59b766b2f929caa5beb9
Elves Rodrigues <elvesmateusrodrigues@outlook.com>
2021-03-09 16:06:24 -0300

    Adição de docstrings

--- data/cleaner.py
+++ data/cleaner.py
@@ -1,21 +1,51 @@
+'''Código responsável por processar as planilhas e gerar uma entrada padrão para o coletor a partir das diferentes fontes.
+'''
+
 from glob import glob
 import json
 import re 
 
 import pandas as pd 
+from pandas.core.frame import DataFrame
 from bs4 import BeautifulSoup
 
 CLEANR = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
 def clean_title(title: str) -> str:
+    '''Realiza o parsing do título do artigo como html e remove alguns outros ruídos
+    Args:
+        - title: Título do artigo.
+    
+    Returns:
+        Retorna o título do artigo com a remoção de ruídos.
+    '''
     s = BeautifulSoup(title, 'lxml').getText()
     s = re.sub(CLEANR, '', s)
     s = s.replace('       ', ' ')
     return s
 
 def get_query(title: str, first_author_last_name: str) -> str:
+    '''Retorna uma consulta padronizada
+    Args:
+        - title: Título do artigo.
+        - first_author_last_name: Sobrenome do primeiro autor.
+    
+    Returns:
+        Retorna a consulta a ser coletada. 
+
+    '''
     return f'{title} {first_author_last_name}'.capitalize()
 
-def process_dataframe(df, filename: str, title: str, save_df_as_csv: bool = True):
+def process_dataframe(df: DataFrame, filename: str, title: str, save_df_as_csv: bool = True):
+    '''Responsável por criar as consultas padronizadas para o coletor, a partir das planilhas.
+    
+    Args:
+        - df: Dataframe com os dados a serem processados.
+        - filename: Nome do arquivo fonte.
+        - title: Variável que representa qual é o título do artigo.
+        - save_df_as_csv: Se é para salvar o dataframe como csv (default True)
+    
+    '''
+    
     if 'ID' not in df:
         df['ID'] = range(1, df.shape[0] + 1)
 
@@ -37,6 +67,12 @@ def process_dataframe(df, filename: str, title: str, save_df_as_csv: bool = True
         df.to_csv(f'csv/{filename}.csv')
 
 def process_files(files: list):
+    '''Organiza as diversas planilhas para serem processadas.
+
+    Args:
+        - files: Lista com o caminho da planilha.
+
+    '''
     with open('raw/titles.json') as f:
         titles = json.loads(f.read())
 
--- src/main.py
+++ src/main.py
@@ -10,7 +10,12 @@ from stem.control import Controller
 
 from scholarly import ProxyGenerator, scholarly
 
+TOR_PASSWORD = 'scholarly_password'
+
+# Número máximo de vezes que se irá tentar realizar uma coleta.
 MAX_ATTEMPTS = 30
+
+# Número máximo de resultados da pesquisa. (10 corresponderia a primeira página)
 MAX_NUM_RESULTS = 10
 
 LOG_PATH = '../log/'
@@ -18,13 +23,23 @@ PROCESSEDS_PATH = LOG_PATH + 'processeds.txt'
 ERROR_PATH = LOG_PATH + 'errors.txt'
 DUMP_PATH = '../dump/'
 
-def renew_tor_ip():
+def renew_tor_ip() -> None:
+    '''Altera o IP do Tor. 
+    '''
+
     with Controller.from_port(port=9051) as controller:
-        controller.authenticate(password="scholarly_password")
+        controller.authenticate(password=TOR_PASSWORD)
         controller.signal(Signal.NEWNYM)
+
+    # Espera um tempo aleatório para que a mudança de IP surta efeito. 
     time.sleep(random.randint(5, 17))
 
 def get_queries_to_crawl() -> list:
+    '''Retorna as queries que não foram processadas.
+
+    Returns:
+        Retorna uma lista de consultas a serem coletadas.
+    '''
     processeds = set()
 
     if os.path.exists(PROCESSEDS_PATH):
@@ -47,6 +62,8 @@ def get_queries_to_crawl() -> list:
     return queries 
 
 def create_folders():
+    '''Cria as pastas básicas para salvamentos dos arquivos de logs e coleta.
+    '''
     if not os.path.exists(DUMP_PATH):
         os.makedirs(DUMP_PATH)
 
@@ -56,8 +73,12 @@ def create_folders():
 if __name__ == '__main__':
     create_folders()
 
+    # Utilizar o Tor com Proxie para que a coleta continue, mesmo após bloqueio de IP pelo Google.
+    # Obs.: Tor precisa estar devidamente configurado. Mais detalhes aqui:
+    #   - https://scholarly.readthedocs.io/en/latest/quickstart.html#pg-tor-external-tor-sock-port-int-tor-control-port-int-tor-password-str
+    #   - https://github.com/scholarly-python-package/scholarly/blob/master/setup_tor.sh
     pg = ProxyGenerator()
-    pg.Tor_External(tor_sock_port=9050, tor_control_port=9051, tor_password="scholarly_password")
+    pg.Tor_External(tor_sock_port=9050, tor_control_port=9051, tor_password=TOR_PASSWORD)
     scholarly.use_proxy(pg)
 
     queries = get_queries_to_crawl()
@@ -80,18 +101,33 @@ if __name__ == '__main__':
                     if order > MAX_NUM_RESULTS:
                         break
                     
+                    # remove objetos do dicionário que não são possíveis de fazer parsing em json
                     del result['container_type']
                     del result['source']
 
+                    # melhora a formataçõa do dicionário final.
                     bib = result['bib']
                     del result['bib']
                     result.update(bib)
 
+                    # insere metadados sobre a coleta
+
+                    # tabela fonte da coleta. P.e.: Artigo, capítulo de livro...
                     result['source_table'] = source
+
+                    # Id da publicação na tabela fonte da coleta.
                     result['source_id'] = sid
+                    
+                    # a consulta que gerou o resultado
                     result['query'] = query
+
+                    # a ordem do resultado no ranking do Google para a consulta
                     result['order_in_results'] = order
+
+                    # Distâncida de edição absoluta entre o título original da publicação e o resultado do Google
                     result['edit_dist_between_titles'] = distance.levenshtein(title, result.get('title', ''))
+
+                    # Distância de edição normalizado entre 0 e 1 (distância de edição absoluta divididade pelo tamanho da maior sequência (resultado do Google ou título original))
                     result['normalized_edit_dist_between_titles'] = distance.levenshtein(title, result.get('title', ''), normalized=True)
 
                     dump.write(json.dumps(result) + '\n')
@@ -105,6 +141,7 @@ if __name__ == '__main__':
                 print(f'\t[Erro {attempt}/{MAX_ATTEMPTS}]: {e}')
                 print(f'\t\t Esperando por {15 * attempt}s...')
 
+                # Tempo de espera para tentar recoleta aumenta progressivamente
                 time.sleep(15 * attempt)
                 attempt += 1 
 
